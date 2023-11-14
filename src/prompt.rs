056a8ab77d2fa3636962c67545a1e6b3514ec7e6
Riguz Lee <dr@riguz.com>
2023-11-14 14:06:52 +0800

    added tests

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use serde::Deserialize;
 use serde_json::Value;
 
@@ -49,10 +51,25 @@ pub struct Chat {
     pub functions: Option<Vec<Function>>,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct Completion {
+    pub version: String,
+    pub engine: String,
+    pub prompt: String,
+    pub parameters: Option<Vec<Parameter>>,
+}
+
 impl Chat {
-    pub fn from(json: &str) -> Chat {
-        let prompt: Chat = serde_json::from_str(&json).unwrap();
-        prompt
+    pub fn from(json: &str) -> Result<Chat, io::Error> {
+        let prompt: Chat = serde_json::from_str(&json)?;
+        Ok(prompt)
+    }
+}
+
+impl Completion {
+    pub fn from(json: &str) -> Result<Completion, io::Error> {
+        let prompt: Completion = serde_json::from_str(&json)?;
+        Ok(prompt)
     }
 }
 
@@ -61,7 +78,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_deserialize_basic_chat() {
+    fn test_should_return_result_when_parse_baisc_chat() {
         let json = r#"  
         {
             "$schema": "../schema/chat-schema.json",
@@ -82,8 +99,109 @@ mod tests {
           }
         "#;
 
-        let prompt = Chat::from(&json);
+        let prompt = Chat::from(json).unwrap();
         assert_eq!(prompt.version, "0.2");
         assert_eq!(prompt.engine, "chat-bison");
+    }
+
+    #[test]
+    fn test_should_return_result_when_parse_chat_if_contain_context() {
+        let json = r#"  
+        {
+            "$schema": "../schema/chat-schema.json",
+            "version": "0.2",
+            "engine": "chat-bison",
+            "messages": [
+              {
+                "role": "user",
+                "content": "Write a hello world in js"
+              }
+            ],
+            "parameters": [
+              {
+                "name": "temperature",
+                "value": 0.1
+              }
+            ]
+          }
+        "#;
+
+        let prompt = Chat::from(json).unwrap();
+        assert_eq!(prompt.version, "0.2");
+        assert_eq!(prompt.engine, "chat-bison");
+
+        let messages = prompt.messages;
+        assert_eq!(messages.len(), 1);
+        assert_eq!(messages[0].role, "user");
+        assert_eq!(messages[0].name, None);
+        assert!(messages[0].function_call.is_none());
+        assert_eq!(
+            messages[0].content,
+            Some("Write a hello world in js".to_string())
+        );
+
+        let parameters = prompt.parameters.unwrap();
+        assert_eq!(parameters.len(), 1);
+        assert_eq!(parameters[0].name, "temperature");
+        assert_eq!(parameters[0].value, 0.1);
+    }
+
+    #[test]
+    fn test_should_return_result_when_parse_baisc_completion() {
+        let json = r#"  
+        {
+            "$schema": "../schema/completion-schema-0.3.json",
+            "version": "0.2",
+            "engine": "text-bison",
+            "prompt": "I'm hungry and I want to",
+            "parameters": [
+                {
+                "name": "temperature",
+                "value": 0.1
+                }
+            ]
+        }
+        "#;
+
+        let prompt = Completion::from(json).unwrap();
+        assert_eq!(prompt.version, "0.2");
+        assert_eq!(prompt.engine, "text-bison");
+        assert_eq!(prompt.prompt, "I'm hungry and I want to");
+        let parameters = prompt.parameters.unwrap();
+        assert_eq!(parameters.len(), 1);
+        assert_eq!(parameters[0].name, "temperature");
+        assert_eq!(parameters[0].value, 0.1);
+    }
+
+    #[test]
+    fn test_should_return_error_when_parse_chat_if_is_not_json() {
+        let json = "!@$!$#";
+
+        let prompt = Chat::from(json);
+        assert!(prompt.is_err());
+    }
+
+    #[test]
+    fn test_should_return_error_when_parse_completion_if_is_not_json() {
+        let json = "!@$!$#";
+
+        let prompt = Completion::from(json);
+        assert!(prompt.is_err());
+    }
+
+    #[test]
+    fn test_should_return_error_when_parse_chat_if_required_filed_is_missing() {
+        let json = "{}";
+
+        let prompt = Chat::from(json);
+        assert!(prompt.is_err());
+    }
+
+    #[test]
+    fn test_should_return_error_when_parse_completion_if_required_filed_is_missing() {
+        let json = "{}";
+
+        let prompt = Completion::from(json);
+        assert!(prompt.is_err());
     }
 }
